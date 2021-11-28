57998b8332b072408480a9de0c0ee713ba947373
Nikita <zhiltsovna@yandexlyceum.ru>
2021-11-28 12:25:02 +0300

    Commit1

---
+++ main.py
@@ -0,0 +1,38 @@
+import sys
+import random
+
+from PyQt5 import uic  # Импортируем uic
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('Ui.ui', self)
+        self.pushButton.clicked.connect(self.run)
+        self.do_paint = False
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_circle(qp)
+            qp.end()
+
+    def draw_circle(self, qp):
+        qp.setBrush(QColor(255, 216, 0))
+        for i in range(1, 100):
+            x = random.randint(1, 75)
+            qp.drawEllipse(random.randint(1, 800), random.randint(1, 800), x, x)
+
+    def run(self):
+        self.do_paint = True
+        self.repaint()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
